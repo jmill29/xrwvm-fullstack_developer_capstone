e7c1f55cb1ff269ab82268ca2771dfbf0133b207
jmill29 <jacoblmiller.jlm@gmail.com>
2024-06-06 13:08:30 -0400

    Format code to pass linting test

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
 # Register your models here.
 
 # CarModelInline class
@@ -9,17 +8,20 @@ class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 5
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-  list_display = ('name', 'type', 'year')
-  list_filter = ['car_make', 'year', 'type']
-  search_fields = ['dealer_id', 'name']
+    list_display = ('name', 'type', 'year')
+    list_filter = ['car_make', 'year', 'type']
+    search_fields = ['dealer_id', 'name']
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
-  inlines = [CarModelInline]
-  list_display = ('name', 'description')
-  search_fields = ['name']
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    search_fields = ['name']
+
 
 # Register models here
 admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,13 +13,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+    # Add code for get requests to back end
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params= params + key + "=" + value + "&"
-    
+            params = params + key + "=" + value + "&"
+
         request_url = backend_url + endpoint + "?" + params
     else:
         request_url = backend_url + endpoint
@@ -29,9 +30,10 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred: {e}")
+
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"/analyze/"+format_query(text)
@@ -45,11 +47,12 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     request_url = backend_url + '/insert_review'
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
